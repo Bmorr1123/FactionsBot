10223ab0d577f2fe452eb93652c42817dfdf5cd1
bmorr1123 <benmorrison1123@gmail.com>
2021-11-30 15:48:01 -0600

    Introduces the Factions cog.

---
+++ factions.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+from pprint import pprint as pp
+
+def check_if_admin(ctx):
+    return ctx.message.author.id in [138027430693568512, 405528235816779776]
+
+
+class Factions(commands.Cog):
+    def __init__(self, bot, data):
+        self.bot = bot
+
+        self.data = data
+
+    # ---------------------------------------------------- Bot Management ----------------------------------------------
+
+    async def set_status(self, string):
+        await self.bot.change_presence(activity=discord.Game(name=string))  # Set Discord status
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Logged in and listening as {self.bot.user}!")
+        await self.set_status("Managing Factions!")
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, arg: int):
+        print(f"Clearing {arg} messages.")
+        messages = await ctx.channel.history(limit=arg + 1).flatten()
+        for message in reversed(messages):
+            await message.delete()
+
+    # ------------------------------------------------------ Account Management ----------------------------------------
+
+    # --------------------------------- User Commands --------------------------------
+
+    @commands.command(aliases=["l"])
+    async def leave(self, ctx):
+
+        pass
+
+    # --------------------------------- Admin Commands -------------------------------
+
+    @commands.command(aliases=["t"])
+    @commands.check(check_if_admin)
+    async def example(self, ctx, length: int = 0):
+        pass
+
+    # -------------------------------------- Voice Channel Management --------------------------------------------------
+
+    # @commands.has_permissions(manage_channels=True)
+    # @commands.Cog.listener()
+    # async def on_voice_state_update(self, member, before, after):
+    #     # Check if Mafia
+    #     print(f"{member} updated their voice state")
+    #     if after.channel is None or after.channel.name.startswith("Fusion-"):
+    #         print(f"{member} updated their voice state in a fusion category.")
+    #         return
+    #     # Check if moved in
+    #     if after.channel.name.startswith("Fusion-"):
+    #         channel = after.channel
+    #         num = int(channel.name[7:])
+    #         category = channel.category
+    #         # if len(channel.members) > 0:
+    #         #     await self.create_game(channel)
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import discord, psutil, atexit
+import discord, psutil, atexit, json, datetime, os, discord.ext.commands as commands
+from factions import Factions
+
+
+data = {}
+with open("data.json", "r") as file:
+    for key, value in json.load(file):
+        data[key] = value
+
 
 def on_close():
     pass
@@ -6,6 +14,55 @@ def on_close():
 
 def main():
     atexit.register(on_close)
+
+    today = datetime.datetime.today()
+
+    file = open("data.json", "r")
+    backup = open(f"backups/data-{today.year}-{today.month}-{today.day}-{today.hour}_{today.minute}.json", "w+")
+
+    backup.writelines(file.readlines())
+
+    file.close()
+    backup.close()
+
+    # Old backup clearing
+    for file in os.listdir("backups"):
+        nums = [""]
+        for i in file[file.find("-") + 1:file.rfind(".")]:
+            if i == "-" or i == "_":
+                nums[-1] = int(nums[-1])
+                nums.append("")
+            else:
+                nums[-1] += i
+        nums[-1] = int(nums[-1])
+
+        if nums[2] > 2000:
+            nums.insert(0, nums.pop(2))
+
+        backup_date = datetime.datetime(*nums)
+
+        if today - backup_date > datetime.timedelta(7):
+            # print(" ".join([str(num) for num in nums]))
+            os.remove(f"backups/{file}")
+
+    # Configuration Loading
+    config_file = open("config.json", "r")
+    config = json.load(config_file)
+    config_file.close()
+
+    # Bot stuff
+    bot = commands.Bot(command_prefix=config["prefix"])
+
+    # @bot.event
+    # async def on_command_error(ctx, error):
+    #     await ctx.send(error)
+
+    cogs = [Factions(bot, data)]
+    for cog in cogs:
+        bot.add_cog(cog)
+        print(f"Loaded \"{cog.qualified_name}\" cog!")
+
+    bot.run(config["bot_token"])
 
 
 if __name__ == '__main__':
