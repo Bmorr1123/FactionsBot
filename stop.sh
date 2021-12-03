91586623d4634b84129fad2a5e14f25d85fed905
Ben O'Bryan <obryanbw@gmail.com>
2021-12-03 13:49:21 -0600

    simple errors

--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-kill `pgrep -af python | grep factions_bot_main.py | cut -d' ' -f1`kill `pgrep -af python | grep main.py | cut -d' ' -f1`
+kill `pgrep -af python | grep factions_bot_main.py | cut -d' ' -f1`
